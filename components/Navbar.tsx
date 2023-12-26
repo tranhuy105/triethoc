5bdca4028395d5a2b84a2be1d9d823ff6c1c08f1
tranhuy105 <imhuy105@gmail.com>
2023-12-26 22:39:28 +0700

    small fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = ({
   setMode: (str: string) => void;
 }) => {
   return (
-    <div className="bg-zinc-200 h-16 flex gap-4 items-center px-6 sticky w-full top-0 z-50 shadow-md">
+    <div className="bg-zinc-200 h-16 flex gap-2 md:gap-4 items-center px-6 sticky w-full top-0 z-50 shadow-md">
       <div className="text-2xl md:text-4xl font-bold text-gray-600 tracking-tighter leading-10 cursor-pointer">
         TRUONGLON
       </div>
