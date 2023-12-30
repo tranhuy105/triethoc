cc01b5992beba3e7556cd9f5f6fc9a6184ab0b0e
tranhuy105 <imhuy105@gmail.com>
2023-12-30 23:05:46 +0700

    zindex fix

--- components/ResText.tsx
+++ components/ResText.tsx
@@ -24,24 +24,28 @@ const ResText = ({
   // console.log(index, perfect);
 
   return (
-    <p
+    <div
+      className="relative"
       onMouseEnter={onHover}
       onMouseLeave={onLeave}
-      className={cn(
-        "relative font-bold text-sky-950 drop-shadow-md cursor-help",
-        {
-          "bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.600),theme(colors.indigo.300),theme(colors.sky.600),theme(colors.fuchsia.600),theme(colors.sky.600),theme(colors.indigo.300),theme(colors.indigo.600))] bg-[length:200%_auto] animate-gradient font-extrabold drop-shadow-lg cursor-default":
-            perfect?.includes(index - 1),
-        }
-      )}
     >
-      {str}
+      <p
+        className={cn(
+          "font-bold text-sky-950 drop-shadow-md cursor-help sticky  w-full z-20",
+          {
+            "bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.600),theme(colors.indigo.300),theme(colors.sky.600),theme(colors.fuchsia.600),theme(colors.sky.600),theme(colors.indigo.300),theme(colors.indigo.600))] bg-[length:200%_auto] animate-gradient font-extrabold drop-shadow-lg cursor-default z-40":
+              perfect?.includes(index - 1),
+          }
+        )}
+      >
+        {str}
+      </p>
       {hover && !perfect?.includes(index - 1) && (
-        <p className="absolute bg-yellow-300/90 top-0 w-full z-30 text-muted-foreground transition px-2 py-1 rounded-md font-bold">
+        <p className="absolute duration-300 bg-yellow-300/90 top-0 w-full z-40 text-muted-foreground transition px-2 py-1 rounded-md font-bold">
           {aws}
         </p>
       )}
-    </p>
+    </div>
   );
 };
 export default ResText;
